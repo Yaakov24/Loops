e5f5c941e0e921f80ef75b767cfef6f1903cb76e
YaakovG <ygoldenbergyo@gmail.com>
2024-03-28 17:13:46 -0400

    15 loops in different ways

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿namespace fifteenloops
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = new int[] { 12, 32, 34, 56, 32, 67, 42, 67, 85, 94, 73 };
+            foreach (int i in num)
+            {
+                Console.WriteLine(i);
+            };
+            for (int i = 0; i <= num.Length - 1; i++)
+            {
+                Console.WriteLine(num[i]);
+            };
+            for (int i = 0; i <= num.Length -1 ; i++)
+            {
+                Console.WriteLine(i);
+            };
+            for (int i = num.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(num[i]);
+            };
+            for (int i = 0; i <= num.Length - 1; i++)
+            {
+                Console.WriteLine(i);
+            };
+            for (int i = 4; i <= num.Length - 1; i++)
+            {
+                Console.WriteLine(num[i]);
+            };
+            for (int i = 0; i <= num.Length - 1; i++)
+            {
+                Console.WriteLine(i);
+            };
+            for (int i = 5; i <= num.Length - 1; i++)
+            {
+                Console.WriteLine(num[i]);
+            };
+            for (int i = 0; i <= num.Length - 1; i++)
+            {
+                Console.WriteLine(i);
+            };
+            for (int i = 0; i <= num.Length - 1; i++)
+            {
+                Console.WriteLine(num[i]);
+            };
+            for (int i = 0; i <= num.Length - 1; i++)
+            {
+                Console.WriteLine(i);
+            };
+            for (int i = 0; i <= num.Length -1; i++)
+            {
+                Console.WriteLine(num[i]);
+            };
+            for (int i = 0; i < num.Length - 1; i++)
+            {
+                Console.WriteLine(i);
+            };
+            for (int i = 0; i <= num.Length - 1; i++)
+            {
+                Console.WriteLine(num[i]);
+            };
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.WriteLine(i);
+            };
+            for (int i = 5; i <= num.Length - 1; i++)
+            {
+                Console.WriteLine(i);
+            };
+            for (int i = 3; i <= num.Length - 1; i++)
+            {
+                Console.WriteLine(num[i]);
+            };
+        }
+    };
+};
